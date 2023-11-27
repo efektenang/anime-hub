9b8086dbc11427fb558d2283b981d2409938fa51
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-11-27 15:57:53 +0700

    fix mapping data

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function AnimeList({api}) {
     return (
         <div className="grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-            {api.data.map((anime, index) => {
+            {api.data?.map((anime, index) => {
                 return (
                 <Link href={`/populer/${anime.mal_id}`} className="cursor-pointer" key={index}>
                     <Image src={anime.images.webp.image_url} width={350} height={350} alt="..." className="w-full max-h-64 object-cover"/>
