03ef3ccbb2ea4534443cdffb1fbcdd5ed7b89b17
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-12-01 16:15:59 +0700

    add new detail anime

--- src/app/populer/[animeId]/page.jsx
+++ src/app/populer/[animeId]/page.jsx
@@ -1,7 +1,25 @@
-export default function Details() {
+import { getAnimeResponse } from "@/app/libs/api-libs"
+import Image from 'next/image'
+
+export default async function Details({ params: { animeId } }) {
+    const anime = await getAnimeResponse(`anime/${animeId}`)
     return (
-        <div>
-            ini adalah halaman detail
-        </div>
+        <>
+            <div className="card lg:card-side bg-base-100 shadow-xl mx-4 my-4">
+                <figure className="md:w-full"><img src={anime.data.images.webp.image_url} alt="Album"/></figure>
+                <div className="card-body">
+                    <h2 className="card-title md:text-2xl">{anime.data.title} - {anime.data.year}</h2>
+                    <div className="flex flex-row">
+                        <div className="badge badge-success mx-1 py-2">Rank #{anime.data.rank}</div>
+                        <div className="badge badge-warning mx-1 py-2">⭐ {anime.data.score}</div>
+                        <div className="badge badge-error mx-1 py-2">{anime.data.rating}</div>
+                    </div>
+                    <p>{anime.data.synopsis}</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Listen</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
