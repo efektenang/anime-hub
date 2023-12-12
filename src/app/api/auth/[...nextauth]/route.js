9df4b6368e14ec8aa92981e3b903b978afe436f0
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-12-12 15:26:27 +0700

    add new auth option for googleAuth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,18 @@
 import NextAuth from 'next-auth'
 import githubAuth from 'next-auth/providers/github'
+import googleAuth from 'next-auth/providers/google'
 
 export const authOption = {
+    // providers: [
+    //     githubAuth({
+    //         clientId: process.env.GITHUB_ID,
+    //         clientSecret: process.env.GITHUB_SECRET
+    //     })
+    // ],
     providers: [
-        githubAuth({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+        googleAuth({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
     secret: process.env.NEXTAUTH_SECRET
