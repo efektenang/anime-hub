e54ac202162f9f875c4ef8bfa78ce05fdd4de099
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-12-13 15:04:43 +0700

    fix text colors

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -2,7 +2,7 @@ export default function HeaderMenu({title}) {
   return (
     <div>
       <div className="p-8">
-        <div className="text-center text-2xl text-white">{title}</div>
+        <div className="text-center text-2xl">{title}</div>
       </div>
     </div>
   )
--- src/components/utilities/Search.jsx
+++ src/components/utilities/Search.jsx
@@ -20,7 +20,7 @@ export default function Search() {
 
   return (
       <>
-        <div className="join">
+        <div className="join text-black">
             <input className="input md:input-md input-sm input-bordered join-item w-[125px]" ref={searchRef} placeholder="Search" onKeyDown={handleSearch} />
             <button onClick={handleSearch} className="btn md:btn-md btn-sm join-item"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg></button>
         </div>
