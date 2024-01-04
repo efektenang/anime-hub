2133961ca6f2934fabcfc1940e25aa181c163e8d
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-01-04 16:20:06 +0700

    fix password validation and error message

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -10,23 +10,27 @@ export default function Register() {
     name: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
+  const isDisabled =
+    data.password !== data.confirmPassword || data.password === "";
 
   const registerUser = async (e) => {
     e.preventDefault();
-    await fetch("/api/register", {
+    const response = await fetch("/api/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    }).then(({ ok, err }) => {
-      if (ok) {
-        router.push("/auth/login");
-      } else {
-        return alert("Email pengguna sudah dipakai!");
-      }
     });
+
+    const res = await response.json();
+    if (res.error === undefined) {
+      return router.push("/auth/login");
+    } else {
+      return alert(res.error);
+    }
   };
   return (
     <div>
@@ -97,22 +101,27 @@ export default function Register() {
                 </div>
                 <div>
                   <label
-                    htmlFor="confirm-password"
+                    htmlFor="confirmPassword"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Confirm password
                   </label>
                   <input
                     type="password"
-                    name="confirm-password"
-                    id="confirm-password"
+                    name="confirmPassword"
+                    id="confirmPassword"
                     placeholder="••••••••"
+                    value={data.confirmPassword}
+                    onChange={(e) => {
+                      setData({ ...data, confirmPassword: e.target.value });
+                    }}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  disabled={isDisabled}>
                   Create an account
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
